8c39655e1c4b1ce82129c39369455af0a94f1ef2
jagadishprasanna <jagadish.yadavadri@gmail.com>
2020-05-28 23:56:54 -0700

    added test

--- contact-tracing-app/src/Implementations/EventSource.ts
+++ contact-tracing-app/src/Implementations/EventSource.ts
@@ -1,10 +1,12 @@
 import { injectable } from "inversify";
 import { IEventSink } from "../Interfaces/IEventSource";
 import * as eventData from "../Interfaces/IEventSource";
+import config from "config";
 import Axios from 'axios';
 import "reflect-metadata";
 import * as _ from "lodash";
 import { String, StringBuilder } from 'typescript-string-operations';
+import "../lib/env";
 
 export module EventSink{
 
@@ -12,12 +14,19 @@ export module EventSink{
 
   @injectable()
   export class EventSource implements IEventSink.IEventSource {
-  
-    name: string = "eventRecorded";
-   // evt: IEventSink.eventData;
    
+    
+    
+    name: string = "eventRecorded";
+    
     public recordEvent(event: IEventSink.eventData): any {
 
+      console.log(`Current NODE_ENV is ${process.env.NODE_ENV}`)
+
+      console.log(`Sample key is ${process.env.SAMPLE_KEY}`)
+
+      let kafka_souce  = process.env.KAFKA_SOURCE;
+      console.log(kafka_souce);
       let val = String.Format("INSERT INTO eventLog (eventID, userID, placeID, eventTime) VALUES ('{0}', '{1}', '{2}','{3}');", event.eventID,event.userID,event.placeID,event.eventTime);
       console.log(val);
       const postEvent = () => {
--- contact-tracing-app/src/main1.ts
+++ contact-tracing-app/src/main1.ts
@@ -3,8 +3,30 @@ import { Service } from "./service";
 import DIContainer from "./di-container";
 import { EventSink } from "./Implementations/EventSource";
 import { IEventSink } from "./Interfaces/IEventSource";
+//import * as dotenv from "dotenv";
+import { resolve } from "path";
+
+import { config } from "dotenv";
+
 
 const service: Service = DIContainer.resolve<Service>(Service);
 const eventSink: EventSink.EventSource = DIContainer.resolve<IEventSink.IEventSource>(EventSink.EventSource);
 console.log(service.getAllNames());
 //console.log(eventSink.recordEvent());
+
+
+import "./lib/env"
+
+console.log(`Current NODE_ENV is ${process.env.NODE_ENV}`)
+
+console.log(`Sample key is ${process.env.SAMPLE_KEY}`)
+
+
+
+//config({ path: resolve(__dirname, "../../.env.dev")});
+ 
+
+//console.log(`Current NODE_ENV is ${process.env.NODE_ENV}`);
+
+//console.log(`Sample key is ${process.env.SAMPLE_KEY}`);
+
